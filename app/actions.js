a442a1f049531c14c67f9d96c839fa8c51bd1402
李龙 <lilong2576@hanweb.com>
2025-04-10 15:47:17 +0800

    feat: zod数据校验

--- app/actions.js
+++ app/actions.js
@@ -3,31 +3,44 @@
 import { redirect } from 'next/navigation'
 import {addNote, updateNote, delNote} from '@/lib/redis';
 import { revalidatePath } from 'next/cache';
+import { z } from "zod";
+
+const schema = z.object({
+  title: z.string().min(1, '请填写标题').max(50, '标题字数最多 20'),
+  content: z.string().min(1, '请填写内容').max(100, '字数最多 100')
+});
 
 
 export async function saveNote(prevState, formData) {
+  // 获取 noteId
   const noteId = formData.get('noteId')
-
-  const data = JSON.stringify({
+  const data = {
     title: formData.get('title'),
     content: formData.get('body'),
     updateTime: new Date()
-  })
-
-
-
-  if (noteId) {
-    updateNote(noteId, data)
-    revalidatePath('/', 'layout')
-
-  } else {
-    const res = await addNote(data)
-    revalidatePath('/', 'layout')
-
   }
+
+  // 校验数据
+  const validated = schema.safeParse(data)
+  if (!validated.success) {
+    return {
+      errors: validated.error.issues,
+    }
+  }
+
+
+
+  // 更新数据库
+  if (noteId) {
+    await updateNote(noteId, JSON.stringify(data))
+    revalidatePath('/', 'layout')
+  } else {
+    await addNote(JSON.stringify(data))
+    revalidatePath('/', 'layout')
+  }
+  
   return { message: `Add Success!` }
 }
-
 export async function deleteNote(prevState, formData) {
   const noteId = formData.get('noteId')
   delNote(noteId)
--- components/NoteEditor.js
+++ components/NoteEditor.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import NotePreview from '@/components/NotePreview'
 import { useActionState } from 'react'
 import { deleteNote, saveNote } from '../app/actions'
@@ -9,6 +9,7 @@ import DeleteButton from '@/components/DeleteButton'
 
 const initialState = {
   message: null,
+  errors: null
 }
 
 export default function NoteEditor({
@@ -21,19 +22,24 @@ export default function NoteEditor({
 
   const [title, setTitle] = useState(initialTitle)
   const [body, setBody] = useState(initialBody)
-  
+
   const isDraft = !noteId
 
   return (
     <div className="note-editor">
       <form className="note-editor-form" autoComplete="off">
+        <input type="hidden" name="noteId" value={noteId || ''} />
         <div className="note-editor-menu" role="menubar">
-          <input type="hidden" name="noteId" value={noteId} />
           <SaveButton formAction={saveFormAction} />
           <DeleteButton isDraft={isDraft} formAction={delFormAction} />
         </div>
         <div className="note-editor-menu">
-          { saveState?.message }
+          {saveState?.message && (
+            <div className="success-message">{saveState.message}</div>
+          )}
+          {saveState?.errors && (
+            <div className="error-message">{saveState.errors[0].message}</div>
+          )}
         </div>
         <label className="offscreen" htmlFor="note-title-input">
           Enter a title for your note
