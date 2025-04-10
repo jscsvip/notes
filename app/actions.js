74e4cde4ca119e22498fb4a8e2550616e180ba22
李龙 <lilong2576@hanweb.com>
2025-04-10 15:15:12 +0800

    feat: useActionState 与 useFormStatus根据 form action 的结果更新表单状态

--- app/actions.js
+++ app/actions.js
@@ -2,9 +2,10 @@
 
 import { redirect } from 'next/navigation'
 import {addNote, updateNote, delNote} from '@/lib/redis';
+import { revalidatePath } from 'next/cache';
 
 
-export async function saveNote(formData) {
+export async function saveNote(prevState, formData) {
   const noteId = formData.get('noteId')
 
   const data = JSON.stringify({
@@ -13,17 +14,23 @@ export async function saveNote(formData) {
     updateTime: new Date()
   })
 
+
+
   if (noteId) {
     updateNote(noteId, data)
-    redirect(`/note/${noteId}`)
+    revalidatePath('/', 'layout')
+
   } else {
     const res = await addNote(data)
-    redirect(`/note/${res}`)
+    revalidatePath('/', 'layout')
+
   }
+  return { message: `Add Success!` }
 }
 
-export async function deleteNote(formData) {
+export async function deleteNote(prevState, formData) {
   const noteId = formData.get('noteId')
   delNote(noteId)
+  revalidatePath('/', 'layout')
   redirect('/')
 }
